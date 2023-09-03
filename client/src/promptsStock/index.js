57c4854e4e920978092840b56a30f43320bdf556
Eriuden <antoinecarrette@hotmail.fr>
2023-09-03 23:18:27 +0200

    mainParameters

---
+++ client/src/promptsStock/index.js
@@ -0,0 +1,9 @@
+//C'est ici pour stocker des prompts
+
+export const prompts = [
+    'un magicien tout de noir vétu',
+    'un dragon de mer ressemblant à une énorme anguille',
+    'un bodybuilder qui se sert de son huile pour faire de la peinture à l"huile',
+    'deux dinosaures jouant aux échecs',
+    'un ananas chez le coiffeur',
+]
