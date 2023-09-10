07ca6e9f957043c7f56baa3b7352b1b4d2129c09
Eriuden <antoinecarrette@hotmail.fr>
2023-09-10 09:21:32 +0200

    sunday session 2

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -3,6 +3,35 @@ import { v2 as cloudinary} from "cloudinary"
 
 import { PostSchema } from "../models/post.model"
 
-const router = express.Router()
+export const router = express.Router()
 
-export default router
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
+
+//fonction getAllPosts
+
+router.route("/").get(async(req,res)=> {
+
+})
+
+//function création
+router.route("/").post(async(req,res)=> {
+    try {
+        const {name, prompt, photo} = req.body
+        const photoUrl = await cloudinary.uploader.upload(photo)
+
+        const newPost = await PostSchema.create({
+            name,
+            prompt,
+            photo: photoUrl.url,
+        })
+
+        res.status(201).json({success: true, data: newPost })
+    } catch(err) {
+        res.status(500).json({success: false, message: error})
+    }
+})
+
